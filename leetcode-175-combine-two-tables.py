9c63e6c02575df35123ec38bff48e265b51e1bc5
Anindya Dey <anindya.dey@vista.com>
2024-01-12 19:54:15 +0530

    add solution to combine-two-tables

---
+++ leetcode-175-combine-two-tables.py
@@ -0,0 +1,7 @@
+# Problem Link: https://leetcode.com/problems/combine-two-tables/description/?lang=pythondata
+
+import pandas as pd
+
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    return person.merge(address, on='personId', how='left')[['firstName', 'lastName', 'city', 'state']]
